0980d5d479497f8863bddd77df2b25025b88beac
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-08-05 23:28:28 +0530

    JavaScript Printing Ways Added.

---
+++ java_script/java_script_coding_ways/java_script_printing_ways/JavaScriptPrintingWays.js
@@ -0,0 +1,24 @@
+/*
+JavaScript Printing Ways.
+1. console object
+2. documnet object
+3. window object
+4. textContent JavaScript property
+5. innerHTML JavaScript property
+6. innerText JavaScript property
+*/
+
+// 1. console object
+console.log("This JavaScript");
+console.log("first", "second", "third");
+console.warn("this warning printed by console");
+console.error("this is error printed by console");
+console.info("this info printed by console");
+console.log(console);
+console.dir(console);
+
+// 2. document object
+document.write("this is text printed in the body by document object using write() function.<br/>");
+document.writeln("<b>this text printed by document object using writeln() function.</b><br/>");
+document.writeln("FirstContent<br/>");
+document.writeln("<b style='font-size: 32px; color: lightgray; background-color: navy'>SecondContent</b>");
