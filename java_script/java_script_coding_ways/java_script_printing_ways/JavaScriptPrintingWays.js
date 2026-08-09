4d2146dc75c07d18d28c0bade1e6043c9578f12d
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-08-09 23:43:20 +0530

    JavaScript Printing ways added.

--- java_script/java_script_coding_ways/java_script_printing_ways/JavaScriptPrintingWays.js
+++ java_script/java_script_coding_ways/java_script_printing_ways/JavaScriptPrintingWays.js
@@ -18,7 +18,21 @@ console.log(console);
 console.dir(console);
 
 // 2. document object
-document.write("this is text printed in the body by document object using write() function.<br/>");
-document.writeln("<b>this text printed by document object using writeln() function.</b><br/>");
-document.writeln("FirstContent<br/>");
-document.writeln("<b style='font-size: 32px; color: lightgray; background-color: navy'>SecondContent</b>");
+// document.write("this is text printed in the body by document object using write() function.<br/>");
+// document.writeln("<b>this text printed by document object using writeln() function.</b><br/>");
+// document.writeln("FirstContent<br/>");
+// document.writeln("<b style='font-size: 32px; color: lightgray; background-color: navy'>SecondContent</b>");
+
+//3. window object
+
+// window.document.write("this text is printed by window object");
+window.console.log("this text is printed by window object");
+
+
+//4.innerHTML java script property
+document.getElementById("heading").innerHTML="<b style='color: red;'>Hi, this is JS inseted HTML code.</b>";
+
+//5. innerText java script property.
+document.getElementById("block_one").innerText="<b style='color: red;'>this is inner text property of JS</b> (this property consider HTML tag as a text only.)";
+
+//6. innerContent JavaScript property.
