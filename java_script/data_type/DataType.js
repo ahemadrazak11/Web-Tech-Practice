8169def6511688d52ef51fc9bc2723cd9fe614a5
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-08-25 16:07:10 +0530

    JavaScript Data Type Added.

---
+++ java_script/data_type/DataType.js
@@ -0,0 +1,11 @@
+var a = 10;
+document.write("var a = " + a + "<br/>");
+
+var a = 100;
+
+let b = 10;
+document.write("let b = " + b + "<br/>");
+
+
+const c = 20;
+document.write("const c = " + c + "<br/>");
