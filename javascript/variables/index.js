455358d9218766aeec41c154aaf309337134dde1
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-09 23:08:36 +0530

    JavaScript Variables Completed.

---
+++ javascript/variables/index.js
@@ -0,0 +1,48 @@
+// var
+/*
+var a;
+a = 10;
+console.log(a);
+
+a = 20;
+console.log(a);
+
+
+let b;
+b = 20;
+console.log(b);
+b = 50;
+console.log(b);
+
+//let b = 20; not allowed
+
+ // const c;  not allowed
+
+ const c = 100; // allowed
+
+ console.log(c);
+
+ // c = 500; // const re initialization not allowed.
+
+ window.document.write("this js");
+
+ /*
+
+ declaration
+ initialization
+ re-declaration
+ re-initialization
+ declaration and initialization in single line
+
+ */
+
+
+//  console.log(a);
+//  var a;
+//  a = 10;
+//  console.log(a);
+
+
+ console.log(b);
+ let b = 10;
+ console.log(b);
