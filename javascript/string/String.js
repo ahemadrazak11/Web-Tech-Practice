1a6ee67bcf436461b14bb5cd0a0a243acaf1a004
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-21 23:07:32 +0530

    String in JavaScript Completed.

---
+++ javascript/string/String.js
@@ -0,0 +1,19 @@
+let str = " Hello ";
+
+console.log("length", str.length);
+
+console.log("index of 'l' is ", str.indexOf("l"));
+console.log(str);
+console.log(str.trim());
+console.log(str.replace("H", "h"));
+console.log(str.replaceAll("l", "X"));
+console.log(str.repeat(3));
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.charAt(3));
+console.log(str.slice(2, 4));
+
+let s = "Java, Python, SQL";
+console.log(s.split(","));
+
+
