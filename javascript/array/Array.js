cd955f81bbfca418c59f4cd6cb4c5597a5f4fd75
ahemadrazak11 <ahemadrazak11@gmail.com>
2026-09-24 23:18:15 +0530

    Array Methods Added.

--- javascript/array/Array.js
+++ javascript/array/Array.js
@@ -1,3 +1,20 @@
 let arr = ['a', 'b', 'c', 'd'];
 
 console.log(arr);
+
+
+// array methods.
+
+let arrNum = [10,20,30,40,50,60,70,80];
+
+arrNum.reverse()
+console.log(arrNum);
+
+console.log(arrNum.slice(1,4));
+
+
+
+console.log(arrNum.splice(1,0, 10,10,10,10));
+
+console.log(arrNum);
+
